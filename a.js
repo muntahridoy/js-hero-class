4b76bd472805cbf4aca8856f2d7b055a72d9ea4f
muntahridoy <muntahridoy@gmail.com>
2025-08-17 00:41:04 +0600

    dfgf

--- a.js
+++ a.js
@@ -942,13 +942,35 @@
 // // unix epoc
 
 // ..............................................
-// swap.js class
+// // swap.js class
 
-let a = 5;
-let b = 7;
+// let a = 5;
+// let b = 7;
 
-console.log(a, b);
-a = b;
-b = a;
+// // console.log(a, b);
+// // a = b;
+// // b = a;
 
-console.log(a, b);
+// const temp = a;
+// a = b;
+// b = temp;
+
+// // console.log(a, b);
+
+// let x = 5;
+// let y = 7;
+
+// // console.log(x, y);
+
+// [x, y] = [y, x];
+// console.log(x, y);
+
+// // ...............................................
+// // milestone  module :21 max.js class
+// if (disa > salman) {
+//  console.log();
+ 
+ 
+// } else {
+ 
+// }
