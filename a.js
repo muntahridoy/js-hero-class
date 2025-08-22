e2686a4213ea0190477189afea7e94821569fbf0
ridoyfarhan <muntahridoy@gmail.com>
2025-08-22 21:07:30 +0600

    sdigf

--- a.js
+++ a.js
@@ -1420,7 +1420,7 @@
 // };
 
 // ...................................................
-// // arguments.js class
+// arguments.js class
 // function add(num1, num2) {
 //   console.log(num1, num2);
 //   console.log(arguments[6]);
@@ -1430,64 +1430,122 @@
 // .......................................................
 // matched.js class
 
-// const numbers = [45, 65, 23, 98, 19];
-// // 1 method
-// // for (let i = 0; i < numbers.length; i++) {
-// //   const number = numbers[i];
-// //   console.log(number);
-// // }
-
-// // 2 method
-// for (const number of numbers) {
+const numbers = [45, 65, 23, 98, 19];
+// 1 method
+// for (let i = 0; i < numbers.length; i++) {
+//   const number = numbers[i];
 //   console.log(number);
 // }
 
-// same way test You failed
-// const dates = ['25-06-2025'];
+// 2 method
+for (const number of numbers) {
+  console.log(number);
+}
 
-// for (const date of dates) {
-//   console.log(date);
+same way test You failed
+const dates = ['25-06-2025'];
+
+for (const date of dates) {
+  console.log(date);
+}
+
+const products = [
+  { id: 1, name: 'dell laptop', price: 12000 },
+  { id: 2, name: 'hp laptop', price: 15000 },
+  { id: 8, name: 'xiaomi phone', price: 22000 },
+  { id: 4, name: 'mac laptop', price: 170000 },
+  { id: 6, name: 'acer laptop', price: 19000 },
+  { id: 7, name: 'samsung phone', price: 25000 },
+  { id: 3, name: 'lenovo laptop', price: 10000 },
+  { id: 9, name: 'oppo phone', price: 19000 },
+  { id: 5, name: 'asus laptop', price: 18000 },
+];
+
+// for (const product of products) {
+//   console.log(product);
 // }
 
-// const products = [
-//   { id: 1, name: 'dell laptop', price: 12000 },
-//   { id: 2, name: 'hp laptop', price: 15000 },
-//   { id: 8, name: 'xiaomi phone', price: 22000 },
-//   { id: 4, name: 'mac laptop', price: 170000 },
-//   { id: 6, name: 'acer laptop', price: 19000 },
-//   { id: 7, name: 'samsung phone', price: 25000 },
-//   { id: 3, name: 'lenovo laptop', price: 10000 },
-//   { id: 9, name: 'oppo phone', price: 19000 },
-//   { id: 5, name: 'asus laptop', price: 18000 },
-// ];
-
-// // for (const product of products) {
-// //   console.log(product);
-// // }
-
-// function matchedProducts(products, search) {
-//   const matched = [];
-//   for (const product of products) {
-//     if (product.name.toLowerCase().includes(search.toLowerCase())) {
-//       matched.push(product);
-//     }
-//   }
-//   return matched;
-// }
-// const result = matchedProducts(products, 'phone');
-// console.log(result);
+function matchedProducts(products, search) {
+  const matched = [];
+  for (const product of products) {
+    if (product.name.toLowerCase().includes(search.toLowerCase())) {
+      matched.push(product);
+    }
+  }
+  return matched;
+}
+const result = matchedProducts(products, 'phone');
+console.log(result);
 
 
+
+// ...............................................................
 // Milestone-5 module 24-1
-
 
 // console.log('25-february-2025');
 
-// Must Research 
+// Must Research
 // string number boolean
-// variable 
+// variable
 // condition
 // array
 // loop
 // object
 // function
+
+// const dates = ['25-06-2025'];
+
+// for (const date of dates) {
+//   // console.log(date);/*  */
+// }
+
+// const dateOf = { date: '25-06-2025' };
+// dateOf.date;
+
+
+const numbers = [45, 65, 23, 98, 19];
+// 1 method
+// for (let i = 0; i < numbers.length; i++) {
+//   const number = numbers[i];
+//   console.log(number);
+// }
+
+// 2 method
+for (const number of numbers) {
+  console.log(number);
+}
+
+same way test You failed
+const dates = ['25-06-2025'];
+
+for (const date of dates) {
+  console.log(date);
+}
+
+const products = [
+  { id: 1, name: 'dell laptop', price: 12000 },
+  { id: 2, name: 'hp laptop', price: 15000 },
+  { id: 8, name: 'xiaomi phone', price: 22000 },
+  { id: 4, name: 'mac laptop', price: 170000 },
+  { id: 6, name: 'acer laptop', price: 19000 },
+  { id: 7, name: 'samsung phone', price: 25000 },
+  { id: 3, name: 'lenovo laptop', price: 10000 },
+  { id: 9, name: 'oppo phone', price: 19000 },
+  { id: 5, name: 'asus laptop', price: 18000 },
+];
+
+// for (const product of products) {
+//   console.log(product);
+// }
+
+function matchedProducts(products, search) {
+  const matched = [];
+  for (const product of products) {
+    if (product.name.toLowerCase().includes(search.toLowerCase())) {
+      matched.push(product);
+    }
+  }
+  return matched;
+}
+const result = matchedProducts(products, 'phone');
+console.log(result);
