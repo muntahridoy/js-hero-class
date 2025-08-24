4abd31bd841d1b5c78b35bba310dd6b2f032536c
ridoyfarhan <muntahridoy@gmail.com>
2025-08-25 01:04:01 +0600

    sdfygeif kjdr

--- b.js
+++ b.js
@@ -1,8 +1,12 @@
-let price = 25;
-let comp_price = price;
+// let price = 25;
+// let comp_price = price;
 
-price = 12; // শুধু price বদলালাম
-comp_price = 20; // শুধু comp_price বদলালাম
+// price = 12; // শুধু price বদলালাম
+// comp_price = 20; // শুধু comp_price বদলালাম
 
-console.log(price); // 12
-console.log(comp_price); // 20
+// console.log(price); // 12
+// console.log(comp_price); // 20
+
+// console.log(document);
+// console.log('Document Object Model');
+
